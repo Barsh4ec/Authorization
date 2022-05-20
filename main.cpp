a73cbc2ddd169d6de60ce6f1aa549f2d479d5022
Orest1234 <konerd12311@gmail.com>
2022-05-20 16:03:07 +0300

    front minesweeper

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Authorization w;
+    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR","1");
     w.resize(600, 700);
     w.show();
     return a.exec();
